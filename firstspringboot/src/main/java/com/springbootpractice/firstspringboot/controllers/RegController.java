7353ec8b5948a0050449b357bfa54cff64d73e81
shiva Concept Solution <50239824+shivaconceptsolution@users.noreply.github.com>
2025-08-30 12:31:39 +0530

    Add files via upload

--- firstspringboot/src/main/java/com/springbootpractice/firstspringboot/controllers/RegController.java
+++ firstspringboot/src/main/java/com/springbootpractice/firstspringboot/controllers/RegController.java
@@ -1,51 +1,44 @@
 package com.springbootpractice.firstspringboot.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Value;
 
 import com.springbootpractice.firstspringboot.models.Reg;
 import com.springbootpractice.firstspringboot.services.RegService;
-import org.springframework.http.ResponseEntity;
-import org.springframework.beans.factory.annotation.Value;
-import java.util.*;;
+
+import java.util.*;
+
 @RestController
 @RequestMapping("/api")
 public class RegController {
 
-@Autowired
-private RegService regService;
+    @Autowired
+    private RegService regService;
 
-@Value("${app.security.username}")
-private String ymlUsername;
+   
 
-@Value("${app.security.password}")
-private String ymlPassword;
-@PostMapping("/reg")
-public Reg AddRegistration(@RequestBody Reg obj)
-{
-	return regService.saveReg(obj);
-}
-@PostMapping("/login")
-public ResponseEntity<Map<String, Object>> LoginOperation(@RequestBody Reg obj)
-{
-	Map<String, Object> response = new HashMap<>();
-	if(regService.verifyReg(obj))
-	{
-		 response.put("status", "success");
-         response.put("message", "Login successful");
-         response.put("username", ymlUsername);
-         response.put("username", ymlPassword);
-	}
-	else
-	{
-		response.put("status", "fail");
-        response.put("message", "Invalid username or password");
-	}
-	return ResponseEntity.ok(response);
-}
+    @PostMapping("/reg")
+    public Reg addRegistration(@RequestBody Reg obj) {
+        return regService.saveReg(obj);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Map<String, Object>> loginOperation(@RequestBody Reg obj) {
+        Map<String, Object> response = new HashMap<>();
+
+       
+        if (regService.verifyReg(obj)) {
+            response.put("status", "success");
+            response.put("message", "Login successful");
+            response.put("username", "admin");
+            response.put("password","admin123"); // fixed key
+        } else {
+            response.put("status", "fail");
+            response.put("message", "Invalid username or password");
+        }
+
+        return ResponseEntity.ok(response);
+    }
 }

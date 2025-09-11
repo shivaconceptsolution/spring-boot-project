4c7857835d2bf31ec223702e9a198bf66abe514e
shiva Concept Solution <50239824+shivaconceptsolution@users.noreply.github.com>
2025-09-11 12:49:15 +0530

    Add files via upload

--- firstspringboot/src/main/java/com/springbootpractice/firstspringboot/services/SecurityConfig.java
+++ firstspringboot/src/main/java/com/springbootpractice/firstspringboot/services/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig {
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http.csrf().disable()
         .authorizeHttpRequests(auth -> auth
-           .requestMatchers("/api/reg","/api/login","/h2-console/**","/myusers").permitAll()
+           .requestMatchers("/api/reg","/api/login","/h2-console/**","/register","/loginuser").permitAll()
            .anyRequest().authenticated())
         .headers(headers -> headers.frameOptions().disable()) // allow H2 console frames
         .httpBasic(Customizer.withDefaults());
